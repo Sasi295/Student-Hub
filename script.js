398d604a037d658a5e91c6ef8320b6f97a76ef11
Sasidhar <sasidhar.madala099@gmail.com>
2025-08-03 17:21:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function checkForUpdates() {
+  const updateMessage = document.getElementById('updateMessage');
+  updateMessage.innerText = "🔔 New papers uploaded for Semester 3!";
+  updateMessage.style.color = "green";
+}
